88c1cd08181903adec027396628cd3a69aa696cc
Clayton O'Neill <claytono@github.com>
2018-04-14 11:15:32 -0400

    Add bash startup scripts

---
+++ bash_profile.sh
@@ -0,0 +1,55 @@
+# OS variables
+[ "$(uname -s)" = "Darwin" ] && export MACOS=1 && export UNIX=1
+[ "$(uname -s)" = "Linux" ] && export LINUX=1 && export UNIX=1
+uname -s | grep -q "_NT-" && export WINDOWS=1
+grep -q "Microsoft" /proc/version 2>/dev/null && export UBUNTU_ON_WINDOWS=1
+
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+pathmunge () {
+        if ! echo "$PATH" | grep -Eq "(^|:)$1($|:)" ; then
+           if [ "$2" = "after" ] ; then
+              PATH="$PATH:$1"
+           else
+              PATH="$1:$PATH"
+           fi
+        fi
+}
+
+AIRPORTCMD="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport"
+if [ -x $AIRPORTCMD ]; then
+  alias airport=$AIRPORTCMD
+fi
+
+
+export GOPATH=$HOME/src/go
+pathmunge $GOPATH/bin
+pathmunge /usr/local/sbin
+pathmunge $HOME/bin
+
+VSCODE_BINDIR="/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+if [ -d "$VSCODE_BINDIR" ]; then
+  pathmunge "$VSCODE_BINDIR" after
+fi
+
+source $(brew --prefix)/etc/bash_completion
+
+if [ $MACOS ]; then
+  export CLICOLOR=1
+  export LSCOLORS=ExFxCxDxBxegedabagacad
+  if command_exists gls; then
+    eval $(gdircolors)
+    alias ls="gls --color=aut"
+  fi
+fi
+
+# check if this is a login and/or interactive shell
+[ "$0" = "-bash" ] && export LOGIN_BASH="1"
+echo "$-" | grep -q "i" && export INTERACTIVE_BASH="1"
+
+# run bashrc if this is a login, interactive shell
+if [ -n "$LOGIN_BASH" ] && [ -n "$INTERACTIVE_BASH" ]; then
+  source ~/.bashrc
+fi
---
+++ bashrc.sh
@@ -0,0 +1,55 @@
+# check if this is a login shell
+[ "$0" = "-bash" ] && export LOGIN_BASH="1"
+
+# run bash_profile if this is not a login shell
+[ -z "$LOGIN_BASH" ] && source ~/.bash_profile
+
+# From: https://github.com/mrzool/bash-sensible/
+# Update window size after every command
+shopt -s checkwinsize
+
+## SANE HISTORY DEFAULTS ##
+
+# Append to the history file, don't overwrite it
+shopt -s histappend
+
+# Save multi-line commands as one command
+shopt -s cmdhist
+
+# Record each line as it gets issued
+PROMPT_COMMAND='history -a'
+
+# Huge history. Doesn't appear to slow things down, so why not?
+HISTSIZE=500000
+HISTFILESIZE=100000
+
+# Avoid duplicate entries
+HISTCONTROL="erasedups:ignoreboth"
+
+# Don't record some commands
+export HISTIGNORE="&:[ ]*:exit:ls:bg:fg:history:clear"
+
+# Useful timestamp format
+HISTTIMEFORMAT='%F %T '
+
+# pc_ = promptcolor_
+pc_lgreen=$(tput setaf 10)
+pc_lblue=$(tput setaf 12)
+pc_lred=$(tput setaf 9)
+pc_reset=$(tput sgr0)
+
+p_user() {
+    if [[ $USER != "coneill" && $USER != "claytono " ]]; then
+      echo "\u@"
+    fi
+}
+
+p_git_branch() {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+prompt_command() {
+  PS1="$(p_user)${pc_lgreen}\h${pc_reset}:${pc_lblue}\w${pc_lred}$(p_git_branch)${pc_reset}\$ "
+}
+PROMPT_DIRTRIM=3
+PROMPT_COMMAND=prompt_command
