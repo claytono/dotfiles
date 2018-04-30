964b5488d74715343aa6629c73d2d125ef7cee3b
Clayton O'Neill <claytono@github.com>
2018-04-30 06:48:46 -0400

    Add initial debian support

--- .bootstrap/bootstrap.sh
+++ .bootstrap/bootstrap.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-set -eixv -o pipefail
+set -exv -o pipefail
 
-# Install Homebrew
-curl -vL https://raw.githubusercontent.com/Homebrew/install/master/install >/tmp/homebrew-install
-ruby /tmp/homebrew-install
+[ "$(uname -s)" = "Darwin" ] && export MACOS=1 && export UNIX=1
+[ "$(uname -s)" = "Linux" ] && export LINUX=1 && export UNIX=1
+
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+if [[ -n "$UNIX" ]]; then
+  NEED=""
+  if ! command_exists git; then
+    NEED="git $NEED"
+  fi
+  if ! command_exists curl; then
+    NEED="curl $NEED"
+  fi
+
+  if [[ "$NEED" != "" ]]; then
+    echo "Need the following binaries available: $NEED"
+    exit 1
+  fi
+fi
+
 
 if [[ ! -d ~/.dotfiles/.git ]]; then
     rm -rf ~/.dotfiles
@@ -17,4 +36,9 @@ alias cfg="git --git-dir=$HOME/.dotfiles --work-tree=$HOME/"
 cfg checkout -f
 cfg config --local status.showUntrackedFiles no
 
-brew bundle --global
+if [[ -n "$MACOS" ]]; then
+  # Install Homebrew
+  curl -vL https://raw.githubusercontent.com/Homebrew/install/master/install >/tmp/homebrew-install
+  ruby /tmp/homebrew-install
+  brew bundle --global
+fi
